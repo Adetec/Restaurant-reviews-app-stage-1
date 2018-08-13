ef5a61a8a2ef156fe6f3beb06cf5cffe784d5541
Adel Lassag <adetech.aadl@gmail.com>
2018-08-13 19:24:12 +0100

    fix: service worker registration

--- js/sw-register.js
+++ js/sw-register.js
@@ -1,7 +1,8 @@
-if (!navigator.serviceWorker) {
-    navigator.serviceWorker.register('sw.js').then(()=>{
-        console.log('Service worker is registred');
-    }).catch(e, ()=> {
-        console.log(e);
-    });
-}
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('js/sw.js').then(()=>{
+            console.log('Service worker is registred');
+        }).catch(e => {
+            console.log(e);
+        });
+        
+    }
