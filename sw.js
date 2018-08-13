a4b4211f3d0f28606d7516f78673be9a6c1716b5
Adel Lassag <adetech.aadl@gmail.com>
2018-08-13 23:09:16 +0100

    feat: creat install cache event for service worker

--- sw.js
+++ sw.js
@@ -1,25 +1,3 @@
-const filesToCache = [
-    './',
-    './index.html',
-    './restaurant.html',
-    './css/styles.css',
-    './img/1.jpg',
-    './img/2.jpg',
-    './img/3.jpg',
-    './img/4.jpg',
-    './img/5.jpg',
-    './img/6.jpg',
-    './img/7.jpg',
-    './img/8.jpg',
-    './img/9.jpg',
-    './img/10.jpg',
-    './js/dbhelper.js',
-    './js/main.js',
-    './js/restaurant_info.js',
-    './sw-register.js',
-    './data/restaurants.json'
-];
-
 let versionOfCache = "restaurants-v1";
 
 self.addEventListener('fetch' , event => {
@@ -36,7 +14,33 @@ self.addEventListener('fetch' , event => {
 });
 
 self.addEventListener('install' , event => {
-
+    event.waitUntil(
+        caches.open(versionOfCache).then(cache=> {
+            return cache.addAll(
+                [
+                    './',
+                    './index.html',
+                    './restaurant.html',
+                    './css/styles.css',
+                    './img/1.jpg',
+                    './img/2.jpg',
+                    './img/3.jpg',
+                    './img/4.jpg',
+                    './img/5.jpg',
+                    './img/6.jpg',
+                    './img/7.jpg',
+                    './img/8.jpg',
+                    './img/9.jpg',
+                    './img/10.jpg',
+                    './js/dbhelper.js',
+                    './js/main.js',
+                    './js/restaurant_info.js',
+                    './sw-register.js',
+                    './data/restaurants.json'
+                ]
+            );
+        })
+    );
 });
 
 self.addEventListener('activate' , event => {
